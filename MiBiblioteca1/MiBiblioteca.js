4be3c832219362a8f4c61fe923741e72eceba0bf
0utKast <educaleft@gmail.com>
2013-11-21 23:53:20 +0000

    cursoMeteor1
    Primera Versión de Biblioteca Meteor

---
+++ MiBiblioteca1/MiBiblioteca.js
@@ -0,0 +1,20 @@
+favoritos = new Meteor.Collection("Favoritos");
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Mis Favoritos";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
